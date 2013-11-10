0c75efa2699e69e20770ece2ffa0705c73141982
Alex.F <gfreezy@gmail.com>
2013-11-10 21:16:52 +0800

    update access_token

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
-var express = require('express');
 var weibo = require('weibo');
-
+var connect = require('connect');
 /**
  * init weibo api settings
  */
@@ -10,44 +9,42 @@ weibo.init('weibo', '1753603767', '51eccdf6f40be550bcbe3aecd55e0ac8');
 /**
  * Create a web application.
  */
-app = express();
-app.get('/', function (req, res, next) {
-  // var user = req.session.oauthUser;
-  var user = {access_token: '2.00R3JhACj8wfuB4beefb80dd3uaClB',blogtype: 'weibo'}
+var app = connect(
+  connect.query(),
+  connect.cookieParser('oh year a cookie secret'),
+  connect.session({ secret: "oh year a secret" }),
+  // using weibo.oauth middleware for use login
+  // will auto save user in req.session.oauthUser
+  weibo.oauth({
+    loginPath: '/login',
+    logoutPath: '/logout',
+    blogtypeField: 'type',
+    afterLogin: function (req, res, callback) {
+      console.log(req.session.oauthUser.screen_name, 'login success');
+      process.nextTick(callback);
+    },
+    beforeLogout: function (req, res, callback) {
+      console.log(req.session.oauthUser.screen_name, 'loging out');
+      process.nextTick(callback);
+    }
+  }),
+  connect.errorHandler({ stack: true, dump: true })
+);
 
+app.use('/', function (req, res, next) {
+  var user = req.session.oauthUser;
   res.writeHeader(200, { 'Content-Type': 'text/html' });
   if (!user) {
-    res.end('Login with <a href="/login?type=weibo">Weibo</a>');
+    res.end('Login with <a href="/login?type=weibo">Weibo</a> | \
+      <a href="/login?type=tqq">QQ</a> | \
+      <a href="/login?type=github">Github</a>');
     return;
   }
   res.end('Hello, <img src="' + user.profile_image_url + '" />\
     <a href="' + user.t_url +
     '" target="_blank">@' + user.screen_name + '</a>. ' +
-    '<a href="/logout">Logout</a>');
-});
-
-app.post('/upload', function(req, res, next) {
-  res.writeHeader(200, { 'Content-Type': 'image/png'});
-  var user = {access_token: '2.00R3JhACj8wfuB4beefb80dd3uaClB',blogtype: 'weibo'}
-
-  var img = '';
-  req.setEncoding('binary');
-  req.on('data', function(chunk) {
-    img += chunk;
-  })
-  req.on('end', function() {
-    weibo.upload(user, 'test', {
-        data: new Buffer(img, 'binary'),
-        name: 'a.png',
-        content_type: 'image/png'}, function(error, status) {
-      if (error) {
-        console.error(error);
-        return;
-      }
-      console.dir(status);
-    });
-  });
-
+    '<a href="/logout">Logout</a>'+
+    'access_token'+user.access_token);
 });
 
 
